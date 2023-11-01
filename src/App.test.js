6bb345c07039b65acb7db557f12a9aecfa286ec4
aeiliff <89414767+aeiliff@users.noreply.github.com>
2023-10-31 21:11:56 -0500

    Updated

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('images and textures', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/new image/i);
+  expect(linkElement).toBeInTheDocument();
+});
