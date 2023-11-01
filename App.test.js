66ab30fea788638fc8515caf8efe0450dda8e2ef
aeiliff <89414767+aeiliff@users.noreply.github.com>
2023-10-31 20:55:34 -0500

    Delete App.test.js

--- App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('images and textures', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/new image/i);
-  expect(linkElement).toBeInTheDocument();
-});
